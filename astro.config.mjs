2ad5efa94a92e3500f4773880505ce365b53161b
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2022-08-10 15:10:48 +0300

    initial commit
    preview version of the new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,16 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap"; // Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-// @ts-check
-// https://astro.build/config
-
-import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://cocotier.ro",
 	integrations: [
+		tailwind(),
 		sitemap({
-			canonicalURL: "https://cocotier.ro",
+			changefreq: "monthly",
+			priority: 0.5,
+			lastmod: new Date(),
 		}),
-		prefetch(),
 	],
+	site: "https://cocotier.ro",
 });
