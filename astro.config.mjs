8e8ec7c4f65f50f1a6c9402c0ec4d94b06c6d18b
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2023-02-18 14:28:41 +0200

    when will the pain end

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,25 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/static";
+import mdx from "@astrojs/mdx";
+
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeFigure from "rehype-figure";
+import remarkCapitalize from "remark-capitalize";
 
 const site =
-	process.env.VERCEL_ENV === "preview"
-		? "https://preview.cocotier.ro"
-		: "https://cocotier.ro";
+	process.env.VERCEL_ENV === "preview" ? "https://preview.cocotier.ro" : "https://cocotier.ro";
 
 // https://astro.build/config
 export default defineConfig({
 	// site: 'https://cocotier.ro',
 	site: site,
 	integrations: [
-		mdx(),
+		mdx({
+			remarkPlugins: [remarkCapitalize],
+			rehypePlugins: [[rehypeAutolinkHeadings, { behavior: "before" }], rehypeFigure],
+		}),
 		sitemap(),
 		prefetch({
 			selector: "a[href^='/']",
@@ -27,4 +32,8 @@ export default defineConfig({
 	adapter: vercel({
 		analytics: true,
 	}),
+	// markdown: {
+	// 	remarkPlugins: [remarkCapitalize],
+	// 	rehypePlugins: [rehypeFigure, [rehypeAutolinkHeadings, { behavior: "before" }]],
+	// },
 });
