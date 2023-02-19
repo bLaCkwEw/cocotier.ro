f2af2b4bc913ba53214ed7b3d3467dc702c36116
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2023-02-19 10:49:31 +0200

    feat: add plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
+// Astro plugins
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import vercel from "@astrojs/vercel/static";
+import serviceWorker from "astrojs-service-worker";
+import compress from "astro-compress";
 import mdx from "@astrojs/mdx";
-
+// Markdown plugins
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeFigure from "rehype-figure";
+import lazyLoadPlugin from "rehype-plugin-image-native-lazy-loading";
 import remarkCapitalize from "remark-capitalize";
 
 const site =
@@ -24,6 +28,8 @@ export default defineConfig({
 			throttle: 3,
 		}),
 		tailwind(),
+		serviceWorker(),
+		compress(),
 	],
 	output: "static",
 	adapter: vercel({
@@ -40,6 +46,7 @@ export default defineConfig({
 					behavior: "wrap",
 				},
 			],
+			lazyLoadPlugin,
 		],
 	},
 });
