0f7f726f2cf62a23c6e9347090a8815a2368039a
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2024-09-15 14:18:29 +0300

    fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,23 +1,3 @@
-// import rss from "@astrojs/rss";
-// import type { AstroAdapter, AstroInstance } from "astro";
-// import { getCollection } from "astro:content";
-
-// export async function get(context) {
-// 	const blog = await getCollection("blog");
-// 	return rss({
-// 		title: "Home - cocotier.ro",
-// 		description: "Welcome to my website!",
-// 		site: context.site,
-// 		items: blog.map((post) => ({
-// 			title: post.data.title,
-// 			pubDate: post.data.date_pub,
-// 			description: post.data.description,
-// 			draft: post.data.draft,
-// 			link: `/blog/${post.slug}/`,
-// 		})),
-// 	});
-// }
-
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import sanitizeHtml from 'sanitize-html';
@@ -27,7 +7,7 @@ const parser = new MarkdownIt();
 export async function GET(context: { site: any }) {
 	const blog = await getCollection("blog");
 	return rss({
-		title: "Home - cocotier.ro",
+		title: "cocotier.ro",
 		description: "Personal website where I write about anything and everything.",
 		site: context.site,
 		items: blog.map((post) => ({
