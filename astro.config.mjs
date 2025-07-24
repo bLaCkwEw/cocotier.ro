d8e65d28f791ef6490b632b0f4cb70ab195f168e
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2025-07-24 21:55:35 +0300

    update deps --- also blocked AI crawlers in robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 // Astro plugins
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 import icon from "astro-icon";
 // Markdown plugins
 import rehypeSlug from "rehype-slug";
@@ -11,42 +11,42 @@ import rehypeFigure from "rehype-figure";
 import remarkCapitalize from "remark-capitalize";
 
 const site =
-	process.env.VERCEL_ENV === "preview" ? "https://preview.cocotier.ro" : "https://cocotier.ro";
+  process.env.VERCEL_ENV === "preview" ? "https://preview.cocotier.ro" : "https://cocotier.ro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: site,
-	output: "static",
-	adapter: vercel(),
-	prefetch: {
-		prefetchAll: true,
-	},
-	integrations: [tailwind(), sitemap(), icon()],
-	markdown: {
-		remarkPlugins: [remarkCapitalize],
-		rehypePlugins: [
-			rehypeFigure,
-			rehypeSlug,
-			[
-				rehypeAutolinkHeadings,
-				{
-					behavior: "append",
-					properties: {},
-					content: {
-						type: "element",
-						tagName: "span",
-						properties: {
-							className: ["mx-2 text-blue-500"],
-						},
-						children: [
-							{
-								type: "text",
-								value: "#",
-							},
-						],
-					},
-				},
-			],
-		],
-	},
+  site: site,
+  output: "static",
+  adapter: vercel(),
+  prefetch: {
+    prefetchAll: true,
+  },
+  integrations: [tailwind(), sitemap(), icon()],
+  markdown: {
+    remarkPlugins: [remarkCapitalize],
+    rehypePlugins: [
+      rehypeFigure,
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          properties: {},
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: ["mx-2 text-blue-500"],
+            },
+            children: [
+              {
+                type: "text",
+                value: "#",
+              },
+            ],
+          },
+        },
+      ],
+    ],
+  },
 });
